7f9bbf27332fb24252a48180fb03713f7631c093
Taylor (Lore) Whittemore <113584473+Lore3581@users.noreply.github.com>
2024-06-19 21:56:52 -0600

    Create date_filters.rb

---
+++ _plugins/date_filters.rb
@@ -0,0 +1,21 @@
+module Jekyll
+  module DateFilters
+    def custom_date_format(date_str)
+      begin
+        date = Date.parse(date_str)
+        case date_str.count('-')
+        when 2
+          date.strftime('%B %d, %Y')  # YYYY-MM-DD format
+        when 1
+          date.strftime('%B %Y')      # YYYY-MM format
+        else
+          date.strftime('%Y')         # YYYY format
+        end
+      rescue
+        date_str  # Return original string if parsing fails
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::DateFilters)
