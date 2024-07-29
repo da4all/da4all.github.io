b5ec42b1cfe2914dc5169ad370fade5541ee506b
Taylor (Lore) Whittemore <113584473+Lore3581@users.noreply.github.com>
2024-07-29 10:59:20 -0600

    Update date_filters.rb

--- _plugins/date_filters.rb
+++ _plugins/date_filters.rb
@@ -1,25 +1,21 @@
-require 'date'
-
 module Jekyll
-  module DateFilters
-    def custom_date_format(date_str)
-      begin
-        case date_str.count('-')
-        when 2
-          date = Date.strptime(date_str, '%Y-%m-%d')
-          date.strftime('%B %d, %Y')  # e.g., January 01, 2023
-        when 1
-          date = Date.strptime(date_str, '%Y-%m')
-          date.strftime('%B %Y')      # e.g., March 2018
-        else
-          date = Date.strptime(date_str, '%Y')
-          date.strftime('%Y')         # e.g., 2023
-        end
-      rescue
-        date_str  # Return the original string if parsing fails
+  module CustomDateFormat
+    def custom_date_format(input)
+      return input unless input.is_a?(String)
+      case input.length
+      when 4
+        Date.strptime(input, '%Y').strftime('%Y')
+      when 7
+        Date.strptime(input, '%Y-%m').strftime('%B %Y')
+      when 10
+        Date.strptime(input, '%Y-%m-%d').strftime('%B %d, %Y')
+      else
+        input
       end
+    rescue ArgumentError
+      input
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::DateFilters)
+Liquid::Template.register_filter(Jekyll::CustomDateFormat)
